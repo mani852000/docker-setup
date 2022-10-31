fddbc43a5bd7770170bd3137aa9b265830382b0a
mani852000 <114754577+mani852000@users.noreply.github.com>
2022-10-31 15:37:34 +0530

    Update env.js

--- angular8-frontend/src/assets/env.js
+++ angular8-frontend/src/assets/env.js
@@ -2,6 +2,6 @@
     window["env"] = window["env"] || {};
   
     // Environment variables
-    window["env"]["apiUrl"] = "http://3.111.187.158:8090";
+    window["env"]["apiUrl"] = "http://3.111.187.158:8090/springboot-prometheus-demo";
     window["env"]["debug"] = true;
   })(this);
